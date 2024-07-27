949d0629767c9ed22c31de171240b9bb7ea3e13f
mhmd <ytheroku64@gmail.com>
2024-07-27 21:01:42 +0300

    Add files via upload

--- webservice/__init__.py
+++ webservice/__init__.py
@@ -1,1 +1,10 @@
+from flask import *
+import os
 
+desired_root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+print(desired_root_path)
+
+routes = Flask(
+    __name__,
+    root_path=desired_root_path
+)
